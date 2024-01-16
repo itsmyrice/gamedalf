0bc75d6074655a66ba9a285c144a1eebc4c63ae5
OguzKabasakal <oguzkabasakal@gmail.com>
2024-01-16 22:01:55 +0100

    try to fix vercel deployment v5

--- pages/search.js
+++ pages/search.js
@@ -1,4 +1,4 @@
-import SearchInput from "@/components/search/SearchInput";
+// import SearchInput from "@/components/search/SearchInput";
 import GameSearchedList from "@/components/search/GameSearchedList";
 
 import { useState } from "react";
@@ -17,8 +17,30 @@ export default function SearchPage() {
 
   return (
     <>
-      <SearchInput searchInput={searchInput} setSearchInput={setSearchInput} />
+      {/* <SearchInput searchInput={searchInput} setSearchInput={setSearchInput} /> */}
+
+      <Input
+      type="text"
+      id="text"
+      placeholder="Insert name"
+      value={searchInput}
+      onChange={(event) => setSearchInput(event.target.value)}
+    />
       <GameSearchedList data={searchedGames} />
     </>
   );
 }
+
+
+const Input = styled.input`
+  height: 4rem;
+  margin-top: 2rem;
+  margin-left: 20%;
+  width: 60%;
+  font-size: 20px;
+  border-radius: 1.5rem;
+  padding: 0.5rem 1.5rem;
+  outline: none;
+  border: none;
+  box-shadow: 3px 3px 5px grey;
+`;
