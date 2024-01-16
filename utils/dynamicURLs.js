88ee9993846238037351264b8ac01526a43c1ff0
OguzKabasakal <oguzkabasakal@gmail.com>
2024-01-16 18:07:20 +0100

    try to fix vercel deployment

--- utils/dynamicURLs.js
+++
@@ -1,8 +0,0 @@
-export const DYNAMIC_URL = {  //different endpoints for fetching different categories
-    hot:{
-      boardgame: "/hot?type=boardgame",
-      rpg : "/hot?type=rpg",
-      videogame : "/hot?type=videogame"
-    }
-  }
-  
