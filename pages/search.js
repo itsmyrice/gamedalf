3436404c86ae355309d4269916b2536796aa625f
OguzKabasakal <oguzkabasakal@gmail.com>
2024-01-16 14:54:38 +0100

    resolve conflict v2

--- pages/search.js
+++ pages/search.js
@@ -1,15 +1,18 @@
-import SearchInput from "../components/search/SearchInput"
+import SearchInput from "components/search/SearchInput.js";
 import GameSearchList from "/components/search/GameSearchedList.js";
+
 import { useState } from "react";
 import useSWR from "swr";
 
 export default function SearchPage() {
   const { data } = useSWR("./api/games");
   const [searchInput, setSearchInput] = useState("");
-  
+
   const searchedGames =
     searchInput.length > 0
-      ? data.filter((game) => `${game.name}`.toLowerCase().includes(searchInput.toLowerCase()))
+      ? data.filter((game) =>
+          `${game.name}`.toLowerCase().includes(searchInput.toLowerCase())
+        )
       : [];
 
   return (
