df0a8f92a009897b4e508874919e43bbd32157be
OguzKabasakal <oguzkabasakal@gmail.com>
2024-01-29 11:11:26 +0100

    separate handleCreate and handleEdit functions v2

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -62,35 +62,6 @@ const StyledUserCreatedGameList = styled.div`
   padding: 3.5rem 0;
 `;
 
-const StyledModal = styled.div`
-  background-color: #f5f5f7;
-  width: 90%;
-  height: 80%;
-  color: black;
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  padding: 20px;
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  z-index: 1000;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  overflow: scroll;
-`;
-
-const Overlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background: rgba(0, 0, 0, 0.7);
-  z-index: 900;
-`;
-
 const StyledButton = styled.button`
   display: inline-flex;
   padding: 10px;
