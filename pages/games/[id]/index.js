b50242dbb5977c90e9cba136f1b7e7cf4a886c15
Glory ann Conwi <gaconwi@googlemail.com>
2024-01-30 20:33:01 +0100

    add more content for the details page

--- pages/games/[id]/index.js
+++ pages/games/[id]/index.js
@@ -3,6 +3,7 @@ import useSWR from "swr";
 import styled from "styled-components";
 import FavouriteButton from "@/components/FavouriteButton";
 import Link from "next/link";
+import { IoIosArrowRoundBack } from "react-icons/io";
 
 export default function DetailsPage({ isFavorite, toggleFavorite }) {
   const router = useRouter();
@@ -18,9 +19,11 @@ export default function DetailsPage({ isFavorite, toggleFavorite }) {
   if (isLoading || error) return <small>loading...</small>;
 
   const checkIsFavorite = isFavorite(game._id);
-
   return (
     <StyledSection>
+      <StyledLink href={"/"}>
+        <IoIosArrowRoundBack />
+      </StyledLink>
       <FavouriteButtonWrapper>
         <FavouriteButton
           toggleFavorite={() => toggleFavorite(game._id)}
@@ -33,11 +36,45 @@ export default function DetailsPage({ isFavorite, toggleFavorite }) {
         width={400}
         height={210}
       ></StyledImageDisplay>
-      <StyledLink href="/">⬅️ Back</StyledLink>
-      <StyledDiv>
-        <StyledYearDisplay>{game.yearpublished}</StyledYearDisplay>
-        <StyledDesciption>{game.description}</StyledDesciption>
-      </StyledDiv>
+      <h1>{game.name}</h1>
+      <StyledRating>{game.rating}</StyledRating>
+      <StyledTable>
+        <thead>
+          <tr>
+            <StyledTh>Age</StyledTh>
+            <StyledTh>Min.Players</StyledTh>
+            <StyledTh2>Max. Players</StyledTh2>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <StyledTd>{game.minAge}</StyledTd>
+            <StyledTd>{game.minPlayers}</StyledTd>
+            <StyledTd2>{game.maxPlayers}</StyledTd2>
+          </tr>
+        </tbody>
+        <thead>
+          <tr>
+            <StyledTh>Year Released</StyledTh>
+            <StyledTh>Playing Time</StyledTh>
+            <StyledTh2>Minimum Playtime</StyledTh2>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <StyledTd>{game.yearpublished}</StyledTd>
+            <StyledTd>{game.playtime}</StyledTd>
+            <StyledTd2>{game.minPlaytime}</StyledTd2>
+          </tr>
+        </tbody>
+      </StyledTable>
+      <StyledContents>Categories</StyledContents>
+      <StyledPTexts>{`${game.categories.join(
+        ","
+      )}`}</StyledPTexts>
+      <StyledContents>Description</StyledContents>
+      <StyledPTexts>{game.description}</StyledPTexts>
+      {game.userCreated ? <p>{game.userCreated}</p> : ""}
     </StyledSection>
   );
 }
@@ -47,52 +84,78 @@ const StyledSection = styled.section`
   flex-direction: column;
   align-items: center;
   background-color: #e6e6fa;
-`;
-
-const StyledImageDisplay = styled.img`
-  margin: 2rem 0;
-  border: 1px inherit black;
-  box-shadow: 2px 3px 5px #ccccff;
-  background-color: #ccccff;
-  border-radius: 1rem;
-  padding: 0.7rem;
-  width: 50%;
-  height: 50%;
-`;
-
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  color: #111111;
-  margin-right: 8rem;
-  word-spacing: 5px;
-  cursor: pointer;
-  &:hover {
-    border-bottom: 1px ridge #ff8200;
-  }
-`;
-
-const StyledDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 1rem;
-`;
-
-const StyledYearDisplay = styled.p`
-  color: #ff8200;
-  font-weight: bold;
-`;
-
-const StyledDesciption = styled.p`
-  color: #111111;
-  border-top: 1px inset black;
-  border-bottom: 1px inset black;
-  padding: 1rem 40px 1rem 1rem;
-  text-align: center;
+  margin-bottom: 5rem;
 `;
 
 const FavouriteButtonWrapper = styled.div`
   position: relative;
-  left: 120px;
-  margin: 10px;
+  left: 42%;
+  padding: 0.3rem 0;
+`;
+
+const StyledImageDisplay = styled.img`
+  margin: 1rem 0;
+  border: 1px inherit black;
+  padding: 0.7rem;
+  width: 90%;
+  height: 70%;
+`;
+
+const StyledLink = styled(Link)`
+  color: #111111;
+  font-size: 2rem;
+  position: relative;
+  margin: 0.5rem 0;
+  right: 45%;
+  &:hover {
+    color: #ff8200;
+    border-left: 1px solid #111111;
+  }
+`;
+
+const StyledRating = styled.p`
+  color: #ff8200;
+  font-weight: bold;
+  margin: 0.5rem 0;
+`;
+
+const StyledTable = styled.table`
+  border-collapse: collapse;
+  text-align: center;
+  display: inline;
+  justify-content: space-between;
+  box-shadow: 1px 0px 4px #8a8686;
+  padding: 0.7rem;
+  margin: 1rem;
+  font-size: 0.9rem;
+`;
+
+const StyledTh = styled.th`
+  border-right: 1px ridge #d8d2d2;
+  padding: 0.7rem;
+`;
+
+const StyledTd = styled.td`
+  padding: 0.7rem;
+  color: #ff8200;
+`;
+
+const StyledTh2 = styled.th`
+  padding: 0.7rem;
+`;
+
+const StyledTd2 = styled.td`
+  color: #ff8200;
+  padding: 0.7rem;
+`;
+
+const StyledContents = styled.h2`
+  font-size: 1.1rem;
+  margin: 1rem 0;
+`;
+const StyledPTexts = styled.p`
+  border-top: 1px inset #d8d2d2;
+  margin: 0 1rem;
+  padding: 1rem 0;
+  font-size: 0.8rem;
 `;
