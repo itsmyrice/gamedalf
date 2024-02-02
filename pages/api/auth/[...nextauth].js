d21cb198bfdc76a01ffde77c1ddbd4df084389cf
Glory ann Conwi <gaconwi@googlemail.com>
2024-02-02 15:48:37 +0100

    fix the github provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   providers: [
-    process.env.VERCEL_ENV === "preview"
-      ? CredentialsProvider({
+    CredentialsProvider({
           name: "Credentials",
           credentials: {
             username: {
@@ -24,13 +23,12 @@ export const authOptions = {
                 "https://images.unsplash.com/photo-1646009760881-fc1173d9d17a?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             };
           },
-        })
-      : GithubProvider({
+        }),
+     GithubProvider({
           clientId: process.env.GITHUB_ID,
           clientSecret: process.env.GITHUB_SECRET,
           redirectUri:
-            process.env.NEXTAUTH_REDIRECT_URI ||
-            "http://localhost:3000/api/auth/callback/github",
+            process.env.NEXTAUTH_REDIRECT_URI
         }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
