286eb42c60aa16ec749bdc010ae2b6ad9ec960d8
OguzKabasakal <oguzkabasakal@gmail.com>
2024-02-02 12:18:25 +0100

    change next auth for vercel

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,15 +1,36 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    // ...add more providers here
+    process.env.VERCEL_ENV === "preview"
+      ? CredentialsProvider({
+          name: "Credentials",
+          credentials: {
+            username: {
+              label: "Username",
+              type: "text",
+              placeholder: "newfishes",
+            },
+            password: { label: "Password", type: "password" },
+          },
+          async authorize() {
+            return {
+              id: 1,
+              name: "New Fishes",
+              email: "new@fishes.com",
+              image:
+                "https://avatars.githubusercontent.com/u/41701911?s=200&v=4",
+            };
+          },
+        })
+      : GithubProvider({
+          clientId: process.env.GITHUB_CLIENT_ID,
+          clientSecret: process.env.GITHUB_CLIENT_SECRET,
+          redirectUri: process.env.NEXTAUTH_REDIRECT_URI || "http://localhost:3000/api/auth/callback/github",
+        }),
   ],
-}
-
-export default NextAuth(authOptions)
+  secret: process.env.NEXTAUTH_SECRET,
+};
+export default NextAuth(authOptions);
